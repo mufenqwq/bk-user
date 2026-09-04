67a54e544fbf6c429441173ff7d46fedee608c24
mufen <wangpan910@gmail.com>
2026-09-04 11:07:43 +0800

    fix: use `StringArrayField` not `ListField`

--- src/bk-user/bkuser/apis/web/data_source/serializers.py
+++ src/bk-user/bkuser/apis/web/data_source/serializers.py
@@ -319,12 +319,10 @@ class DataSourceUpdateInputSLZ(serializers.Serializer):
 
 
 class DataSourceBatchDeleteInputSLZ(serializers.Serializer):
-    data_source_ids = serializers.ListField(
-        help_text="数据源 ID 列表", child=serializers.IntegerField(), allow_empty=False
-    )
+    data_source_ids = StringArrayField(help_text="数据源 ID 列表，逗号分隔", allow_blank=False)
     is_delete_idp = serializers.BooleanField(help_text="重置数据源时是否同时删除 Idp 相关配置", default=False)
 
-    def validate_data_source_ids(self, data_source_ids: List[int]) -> List[int]:
+    def validate_data_source_ids(self, data_source_ids: List[str]) -> List[str]:
         tenant_id = self.context["tenant_id"]
 
         exists_data_source_ids = DataSource.objects.filter(
