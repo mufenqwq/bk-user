40713d5d4c652a2358f8127b880cddde5b8b2696
mufen <wangpan910@gmail.com>
2026-09-21 21:50:13 +0800

    fix: fix typo

--- src/bk-user/bkuser/apis/web/idp/views.py
+++ src/bk-user/bkuser/apis/web/idp/views.py
@@ -151,12 +151,10 @@ class IdpRetrieveUpdateDestroyApi(CurrentUserTenantMixin, generics.RetrieveUpdat
     lookup_url_kwarg = "id"
 
     def get_queryset(self):
-        # Note: 仅可管理实名认证源 & 孤儿认证源
         current_tenant_id = self.get_current_tenant_id()
-        return Idp.objects.filter(
-            owner_tenant_id=current_tenant_id,
-            id__in=IdpDataSourceRelationHandler.get_real_idp_ids_with_orphan(current_tenant_id),
-        )
+        idp_ids = IdpDataSourceRelationHandler.get_real_idp_ids_with_orphan(current_tenant_id)
+
+        return Idp.objects.filter(owner_tenant_id=current_tenant_id, id__in=idp_ids)
 
     @swagger_auto_schema(
         tags=["idp"],
